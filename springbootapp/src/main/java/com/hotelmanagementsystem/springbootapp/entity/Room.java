88185e3b10faf616bcc96a904fb1c57585c2a0fe
Kavee Hewamanna <hewamannakavee@gmail.com>
2025-01-17 10:46:27 +0530

    Create entities (User,Booking,Room)

---
+++ springbootapp/src/main/java/com/hotelmanagementsystem/springbootapp/entity/Room.java
@@ -0,0 +1,44 @@
+package com.hotelmanagementsystem.springbootapp.entity;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "bookings" )
+
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String roomType;
+    private BigDecimal roomPrice;
+    private String roomPhotoUrl;
+    private String roomDescription;
+    private List<Booking> bookings = new ArrayList<>();
+
+    
+    @Override
+    public String toString() {
+        return "Room{" +
+                "id=" + id +
+                ", roomType='" + roomType + '\'' +
+                ", roomPrice=" + roomPrice +
+                ", roomPhotoUrl='" + roomPhotoUrl + '\'' +
+                ", roomDescription='" + roomDescription + '\'' +
+                '}';
+    }
+
+
+}
