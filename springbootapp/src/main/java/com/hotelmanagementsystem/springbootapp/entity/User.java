83da08ac2bf2177e83c8c34935c1f34bab8f6b5f
Kavee Hewamanna <hewamannakavee@gmail.com>
2025-01-17 11:51:32 +0530

    Create DTO's

--- springbootapp/src/main/java/com/hotelmanagementsystem/springbootapp/entity/User.java
+++ springbootapp/src/main/java/com/hotelmanagementsystem/springbootapp/entity/User.java
@@ -9,10 +9,13 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
@@ -37,9 +40,14 @@ public class User implements UserDetails {
     @NotBlank(message = "Phone Number is required")
     private String phoneNumber;
 
+    @NotBlank(message = "Password is required")
     private String password;
+    
     private String role;
+
+    @OneToMany(mappedBy = "user" , fetch = FetchType.LAZY,cascade = CascadeType.ALL )
     private List<Booking> bookings = new ArrayList<>();
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of(new SimpleGrantedAuthority(role));
